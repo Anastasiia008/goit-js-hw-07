d88320defccb484d039022e6476a581edd0f2df8
Anastasiia Tkachenko <96151518+Anastasiia008@users.noreply.github.com>
2023-05-11 22:31:08 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Отримуємо посилання на  елемент гелері в документі
+const galleryList = document.querySelector(".gallery");
+
+// Відображаємо функцію та передаємо їй масив об'єктів
+const galleryItemsMarkup = makeGalleryItemsMarkup(galleryItems);
+
+// Додаємо розмітку в елемент гелері
+galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+// Створюємо функцію,  щоб згенерувати розмітку для гелереї
+function makeGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" 
+        href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+        </a>
+        </li>`;
+    })
+    .join("");
+}
+
+// Запускаємо бібліотеку SimpleLightbox та налаштовуємо її
+const modal = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
